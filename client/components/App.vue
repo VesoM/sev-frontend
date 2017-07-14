66eff36fe275d94f7f2a2c2bb2c5cfb4940713fd
VesoM <veselin.manojlovic@gmail.com>
2017-07-14 13:08:47 +0200

    initial commit

---
+++ client/components/App.vue
@@ -0,0 +1,47 @@
+<template lang='pug'>
+  #app
+    .dv1 crvena
+      | novi red
+    .dv2 plava
+      | novi red
+    .dv3 zelena
+      | novi red
+    router-view
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-size: 2rem;
+  font-family: -apple-system, BlinkMacSystemFont,
+               'avenir next', avenir,
+               helvetica, 'helvetica neue',
+               Ubuntu,
+               'segoe ui', arial,
+               sans-serif;
+}
+.page {
+  text-align: center;
+  /* nesting for the need to test postcss */
+  code {
+    background-color: #f0f0f0;
+    padding: 3px 5px;
+    border-radius: 2px;
+  }
+}
+</style>
+<style lang='sass'>
+  #app
+    height: 100vh
+    width: 100vw
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+  .dv1
+    background-color: blue
+  .dv2
+    background-color: red
+  .dv3
+    background-color: green
+</style>
---
+++ client/views/Login.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  .login-dialog
+    .login-forma
+      .login-title
+        | login
+        .login-close X
+      .login-email: input(type='email', v-model='email', placeholder='Email')
+      .login-password: input(type='password', v-model='password', placeholder='Password')
+      .login-submit: button(@click="login") login
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$http.post('http://localhost:8000/oauth/token')
+        .then(function (response) {
+          console.log(response)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .login-dialog
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    background-color: rgba(0,0,0,0.5)
+    position: fixed
+    height: 100%
+    width: 100%
+  .login-title
+    display: flex
+    flex-direction: row
+    justify-content: space-between
+  .login-close
+    align-self: flex-start
+  .login-forma
+    display: flex
+    flex-direction: column
+    background-color: rgba(255,255,255,1)
+    padding: 20px
+</style>
